cb3bf43e1e71a1f18252e825bfc209ac5006fe06
Michal Graziowski <grazik@Michas-MacBook-Pro-2413.local>
2023-01-07 13:33:10 +0100

    create footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -2,8 +2,14 @@ import styles from "./footer.module.scss";
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <p>Lorem ipsum</p>
+    <footer className={`${styles.footer}`}>
+      <div className={"wrapper"}>
+        <p>Recruitment task for Dynamic Solutions</p>
+        <p>
+          Copyright &#169; 2023{" "}
+          <span className="bold">QualityBit Michal Graziowski</span>
+        </p>
+      </div>
     </footer>
   );
 };
