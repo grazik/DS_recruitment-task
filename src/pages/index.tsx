71219aea60b5c796005f510b9f8dc74324f89326
Michal Graziowski <grazik@Michas-MacBook-Pro-2413.local>
2023-01-07 15:55:51 +0100

    fill HP, add styles for HP, align wrapper maxWidth, prevent header shrinking, add hero asset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import { Inter } from "@next/font/google";
+import { HeroBanner } from "@molecules/HeroBanner/HeroBanner";
+import { LinkCta } from "@atoms/LinkCta/LinkCta";
+import HeroImg from "@public/hero.jpg";
+import styles from "@styles/homePage.module.scss";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <HeroBanner
+        image={{
+          src: HeroImg,
+          alt: "Hero Image",
+        }}
+        title={"Recruitment task for DS"}
+        subtitle={"SPA made in Next.js"}
+        isFullHeight
+      >
+        <div className={styles.heroBannerChildrenWrapper}>
+          <LinkCta href={"/form"} title={"Go to form"} />
+          <LinkCta href={"/profile"} title={"Check your profile"} />
+        </div>
+      </HeroBanner>
+    </>
+  );
 }
