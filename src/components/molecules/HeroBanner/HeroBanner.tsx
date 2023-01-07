b15ae8d6fa44433819ffe7be9228f1c6f32c3490
Michal Graziowski <grazik@Michas-MacBook-Pro-2413.local>
2023-01-07 15:54:57 +0100

    create HeroBanner component

---
+++ src/components/molecules/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,39 @@
+import styles from "./heroBanner.module.scss";
+import Image, { ImageProps } from "next/image";
+import { ReactElement } from "react";
+
+interface HeroBannerProps {
+  image: ImageProps;
+  title?: string;
+  subtitle?: string;
+  children?: ReactElement;
+  isFullHeight?: boolean;
+}
+
+export const HeroBanner = ({
+  image,
+  title,
+  subtitle,
+  children,
+  isFullHeight,
+}: HeroBannerProps) => {
+  return (
+    <section
+      className={`${styles.heroBanner} ${
+        isFullHeight ? styles.fullHeight : ""
+      }`}
+      {...(!!title ? { "aria-labelledby": "heroBanner-title" } : {})}
+    >
+      <Image src={image.src} alt={image.alt} fill={true} objectFit={"cover"} />
+      <div className={styles.contentWrapper}>
+        {title && (
+          <h1 className={styles.title} id={"heroBanner-title"}>
+            {title}
+          </h1>
+        )}
+        {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
+        {children}
+      </div>
+    </section>
+  );
+};
