5c3f1aaba26f190becd72a5bed0733175a21c453
Michal Graziowski <grazik@Michas-MacBook-Pro-2546.local>
2023-01-11 19:36:19 +0100

    escape apos char

--- src/components/molecules/NoData/NoData.tsx
+++ src/components/molecules/NoData/NoData.tsx
@@ -4,7 +4,9 @@ import styles from "./noData.module.scss";
 export const NoData = ({}) => {
   return (
     <div className={styles.noData}>
-      <p>You haven't provided your data, yet. Please do that in the Form</p>
+      <p>
+        You haven&apos;t provided your data, yet. Please do that in the Form
+      </p>
       <LinkCta href={"/form"} title={"Form"} />
     </div>
   );
