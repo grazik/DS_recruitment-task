7a5ba4a1ec4c964228d37a0d15c74614255ebd48
Michal Graziowski <grazik@Michas-MacBook-Pro-2677.local>
2023-01-15 12:37:59 +0100

    improve form accessibility

--- src/components/atoms/FormField/FormField.tsx
+++ src/components/atoms/FormField/FormField.tsx
@@ -26,27 +26,44 @@ export const FormField = (props: FormFieldProps) => {
 
   return (
     <div className={styles.wrapper}>
-      <label
-        className={classNames(styles.label, { [styles.required]: isRequired })}
-      >
+      <label className={classNames(styles.label)} htmlFor={`form-${name}`}>
         {label ?? name}
+        {isRequired && (
+          <>
+            <span className={styles.required} aria-hidden={"true"}>
+              *
+            </span>
+            <span className="sr-only"> - required</span>
+          </>
+        )}
       </label>
       {isInputProps(props) ? (
         <Field
           name={name}
           className={classNames(styles.input, className)}
           type={props.type}
+          aria-describedby={`form-${name}-error`}
+          id={`form-${name}`}
           {...rest}
         />
       ) : (
         <Field
           {...rest}
+          aria-describedby={`form-${name}-error`}
+          id={`form-${name}`}
           className={classNames(styles.input, className)}
           name={name}
           as={props.as}
         />
       )}
-      <ErrorMessage name={name} className={styles.error} component={"span"} />
+      <ErrorMessage
+        name={name}
+        render={(errorMessage) => (
+          <span id={`form-${name}-error`} className={styles.error}>
+            {errorMessage}
+          </span>
+        )}
+      />
     </div>
   );
 };
